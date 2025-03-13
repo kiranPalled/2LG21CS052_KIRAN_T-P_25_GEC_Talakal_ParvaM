807a8a392114be19e548a3727cef91d8e9a398f6
kiran <kiranmp6361@gmail.com>
2025-03-13 15:35:10 +0530

    new

--- 11 Mar 25/studentcrud/src/main/java/com/example/studentcrud/repository/StudentRepository.java
+++ 11 Mar 25/studentcrud/src/main/java/com/example/studentcrud/repository/StudentRepository.java
@@ -1,3 +1,11 @@
-public class StudentRepository {
+package com.example.studentcrud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.studentcrud.model.Student;
+
+
+@Repository
+public interface StudentRepository extends 
+JpaRepository<Student, Long> {
     
 }
